cb93b7e55e0bd2c4205f939e8e4fbf94bfb18beb
Lukas Udstuen <lukas.udstuen@gmail.com>
2022-03-20 14:58:50 -0300

    upload

--- upload.py
+++ upload.py
@@ -2,7 +2,6 @@ import os
 
 from documentcloud import DocumentCloud
 
-
 # We will create a Python script that uploads a document into documentcloud, and then monitors to see that the document has been uploaded successfully.
 # Once it has been uploaded successfully, we can proceed to process the docment with the search term.
 
@@ -15,8 +14,10 @@ def main():
 	print("We have username: ", DOCUMENT_CLOUD_USERNAME)
 	print("WE HAVE URL: ", DOCUMENT_URL)
 	client = DocumentCloud(DOCUMENT_CLOUD_USERNAME, DOCUMENT_CLOUD_PASSWORD)
-	obj = client.documents.upload(DOCUMENT_URL)
-
+	# For some reason, when we download documents via the URL, the service does not process them correctly.
+	pdf_path = os.path.join(os.getcwd(), 'output.pdf')
+	pdf = open(pdf_path, "rb")
+	obj = client.documents.upload(pdf)
 
 if __name__ == "__main__":
 	main()
