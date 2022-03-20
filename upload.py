e30b781950ff73ee0f5a9ed01c72e79970f3b1b3
Lukas Udstuen <lukas.udstuen@gmail.com>
2022-03-20 14:10:10 -0300

    add

---
+++ upload.py
@@ -0,0 +1,20 @@
+import os 
+
+from documentcloud import DocumentCloud
+
+
+# We will create a Python script that uploads a document into documentcloud, and then monitors to see that the document has been uploaded successfully.
+# Once it has been uploaded successfully, we can proceed to process the docment with the search term.
+
+DOCUMENT_CLOUD_USERNAME = os.getenv('DOCUMENT_CLOUD_USERNAME')
+DOCUMENT_CLOUD_PASSWORD = os.getenv('DOCUMENT_CLOUD_PASSWORD')
+DOCUMENT_URL = os.getenv('PDF_URL')
+
+def main(): 
+	print("helloworld") 
+	# client = DocumentCloud(
+
+
+
+if __name__ == "__main__":
+	main()
