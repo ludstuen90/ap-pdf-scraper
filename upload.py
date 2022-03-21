8064e1d4d3b74a184fc1489e1dc469a8fd0ecae7
Lukas Udstuen <lukas.udstuen@gmail.com>
2022-03-20 22:48:07 -0300

    try

--- upload.py
+++ upload.py
@@ -35,32 +35,29 @@ def send_alert(search_term, document_url):
 
 def main(): 
 	client = documentcloud.DocumentCloud(DOCUMENT_CLOUD_USERNAME, DOCUMENT_CLOUD_PASSWORD)
-#	# For some reason, when we download documents via the URL, the service does not process them correctly.
-#	pdf = open(DOCUMENT_PATH, "rb")
-#	obj = client.documents.upload(pdf)
-#	for x in range(0,45):
-#		try:
-#			print("beginning an attempt")
-#			obj.full_text
-#		except (requests.exceptions.HTTPError, documentcloud.exceptions.DoesNotExistError)  as http_error:
-#			sleep(10) # wait 10 seconds before retrying
-#			pass
-#		else:
-#			print("Processing is finished")
-#			print("Proceeding to delete PDF")
-#			break	
-#	
-#	# Examine the full text of the PDF to see if our term can be found. 
-#	# If so, send an email.
-#	if SEARCH_TERM.upper() in obj.full_text.upper():
-#		send_alert(SEARCH_TERM, DOCUMENT_URL)
-#		Alert().lukas("hello")
-#
-#	# Finally, delete our PDF which has been stored Document Cloud.
-#	obj.delete()
-	print("just before alert, ", DC_USERNAME)	
-	CronAddOn().send_mail("TERM MATCHED", f"""Hi there, \n We're writing to let you know that we've discovered the following match: \n\n TERM: {SEARCH_TERM} \n DOCUMENT URL:  {DOCUMENT_URL} \n\n Thanks.""")
+	# For some reason, when we download documents via the URL, the service does not process them correctly.
+	pdf = open(DOCUMENT_PATH, "rb")
+	obj = client.documents.upload(pdf)
+	for x in range(0,45):
+		try:
+			print("beginning an attempt")
+			obj.full_text
+		except (requests.exceptions.HTTPError, documentcloud.exceptions.DoesNotExistError)  as http_error:
+			sleep(10) # wait 10 seconds before retrying
+			pass
+		else:
+			print("Processing is finished")
+			print("Proceeding to delete PDF")
+			break	
+	
+	# Examine the full text of the PDF to see if our term can be found. 
+	# If so, send an email.
+	if SEARCH_TERM.upper() in obj.full_text.upper():
+		print("Sending email to user.")
+		CronAddOn().send_mail("TERM MATCHED", f"""Hi there, \n We're writing to let you know that we've discovered the following match: \n\n TERM: {SEARCH_TERM} \n DOCUMENT URL:  {DOCUMENT_URL} \n\n Thanks.""")
 
+	# Finally, delete our PDF which has been stored Document Cloud.
+	obj.delete()
 
 if __name__ == "__main__":
 	main()
